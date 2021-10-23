c17c3481626de951d6406b96f30032ede3c77f61
Bogdan <bogbuk@gmail.com>
2021-10-23 23:53:52 +0300

    add payment

--- app/Http/Controllers/RouteOrder.php
+++ app/Http/Controllers/RouteOrder.php
@@ -6,6 +6,7 @@ use App\Models\CarRoute;
 use App\Models\CarsRouteOrders;
 use App\Models\PlacesRouteOrders;
 use Illuminate\Http\Request;
+use TheSeer\Tokenizer\Exception;
 
 class RouteOrder extends Controller {
     //
@@ -18,10 +19,14 @@ class RouteOrder extends Controller {
      * @return bool
      */
     public function save( \Illuminate\Http\Request $request ) {
-        $data = $request->all();
+
+        try {
 
 
-        $d = '{
+            $data = $request->all();
+
+
+            $d = '{
             "route_id":1,
             "route_date":"2021.10.24 02:44:00",
             "total":"283.00",
@@ -31,80 +36,83 @@ class RouteOrder extends Controller {
             "cars":[{"id":1,"count":1,"price":250}]
             ,"places":[{"id":3,"duration":null,"price":"33.00"}],"user_id":1}';
 
-        $data['user_id'] = \Auth::user()->id ?? 1;
+            $data['user_id'] = \Auth::user()->id ?? 1;
 
 
-        $routeOrder = new \App\Models\RouteOrder();
+            $routeOrder = new \App\Models\RouteOrder();
 
 
-        $routeOrder->user_id          = $data['user_id'] ?? 0;
-        $routeOrder->email            = $data['order_details']['email'] ?? '';
-        $routeOrder->first_name       = $data['order_details']['first_name'] ?? '';
-        $routeOrder->last_name        = $data['order_details']['last_name'] ?? '';
-        $routeOrder->phone            = $data['order_details']['phone'] ?? '';
-        $routeOrder->comment          = $data['order_details']['comment'] ?? '';
-        $routeOrder->pickup_address   = $data['order_details']['pickup_address'] ?? '';
-        $routeOrder->drop_off_address = $data['order_details']['drop_off_address'] ?? '';
-        $routeOrder->route_id         = $data['route_id'];
-        $routeOrder->route_date       = $data['route_date'];
-        $routeOrder->amount           = $data['total'];
-        $routeOrder->adults           = $data['adults'];
-        $routeOrder->childrens        = $data['childrens'];
-        $routeOrder->luggage          = $data['luggage'];
-        $routeOrder->save();
+            $routeOrder->user_id          = $data['user_id'] ?? 0;
+            $routeOrder->email            = $data['order_details']['email'] ?? '';
+            $routeOrder->first_name       = $data['order_details']['first_name'] ?? '';
+            $routeOrder->last_name        = $data['order_details']['last_name'] ?? '';
+            $routeOrder->phone            = $data['order_details']['phone'] ?? '';
+            $routeOrder->comment          = $data['order_details']['comment'] ?? '';
+            $routeOrder->pickup_address   = $data['order_details']['pickup_address'] ?? '';
+            $routeOrder->drop_off_address = $data['order_details']['drop_off_address'] ?? '';
+            $routeOrder->route_id         = $data['route_id'];
+            $routeOrder->route_date       = $data['route_date'];
+            $routeOrder->amount           = $data['total'];
+            $routeOrder->adults           = $data['adults'];
+            $routeOrder->childrens        = $data['childrens'];
+            $routeOrder->luggage          = $data['luggage'];
+            $routeOrder->save();
 
-        foreach ( $data['cars'] as $c ) {
-            $car = new CarsRouteOrders();
+            foreach ( $data['cars'] as $c ) {
+                $car = new CarsRouteOrders();
 
-            $car->cars_route_order_id = $routeOrder->id;
-            $car->car_id              = $c['id'];
-            $car->count               = $c['count'];
-            $car->price               = $c['price'];
+                $car->cars_route_order_id = $routeOrder->id;
+                $car->car_id              = $c['id'];
+                $car->count               = $c['count'];
+                $car->price               = $c['price'];
 
-            $car->save();
-        }
-
-        foreach ( $data['places'] as $p ) {
-            $place = new PlacesRouteOrders();
-
-            $place->places_route_order_id = $routeOrder->id;
-            $place->place_id              = $p['id'];
-            $place->durations             = $p['duration'];
-            $place->price                 = $p['price'];
-
-            $place->save();
-
-        }
-
-
-        if ( isset($data['payment_type']) && $data['payment_type'] === 1 ) {
-            $stripe = new \Stripe\StripeClient( env( 'STRIPE_SECRET_API' ) );
-
-            $token = $data['stripe_token'];
-
-            $charge = $stripe->charges->create( [
-                'amount'      => (int) $routeOrder->amount * 100,
-                'currency'    => 'usd',
-                'description' => 'Example charge',
-                'source'      => $token,
-                'metadata'    => [ 'order_id' => $routeOrder->id ]
-            ] );
-
-
-            if ( $charge['status'] && $charge['status'] === 'succeeded' ) {
-                $routeOrder->status = 'complete';
-                $routeOrder->save();
-            } else {
-                $routeOrder->status = 'fail';
-                $routeOrder->save();
+                $car->save();
             }
+
+            foreach ( $data['places'] as $p ) {
+                $place = new PlacesRouteOrders();
+
+                $place->places_route_order_id = $routeOrder->id;
+                $place->place_id              = $p['id'];
+                $place->durations             = $p['duration'];
+                $place->price                 = $p['price'];
+
+                $place->save();
+
+            }
+
+
+            if ( isset( $data['payment_type'] ) && $data['payment_type'] === 1 ) {
+                $stripe = new \Stripe\StripeClient( env( 'STRIPE_SECRET_API' ) );
+
+                $token = $data['stripe_token'];
+
+                $charge = $stripe->charges->create( [
+                    'amount'      => (int) $routeOrder->amount * 100,
+                    'currency'    => 'usd',
+                    'description' => 'Example charge',
+                    'source'      => $token,
+                    'metadata'    => [ 'order_id' => $routeOrder->id ]
+                ] );
+
+
+                if ( $charge['status'] && $charge['status'] === 'succeeded' ) {
+                    $routeOrder->status = 'complete';
+                    $routeOrder->save();
+                } else {
+                    $routeOrder->status = 'fail';
+                    $routeOrder->save();
+                }
 
 //            return $charge;
 
+            }
+
+            return true;
+
+        } catch ( Exception $e ) {
+            return $e->getMessage();
         }
-
-        return true;
-
 //        return $data;
     }
 
