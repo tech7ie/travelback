31b66f1e835ae7ab9a5785a8ff34825f7d182772
Bogdan <bogbuk@gmail.com>
2021-10-25 15:43:46 +0300

    add user

--- app/Http/Sections/Users.php
+++ app/Http/Sections/Users.php
@@ -24,8 +24,7 @@ use SleepingOwl\Admin\Section;
  *
  * @see https://sleepingowladmin.ru/#/ru/model_configuration_section
  */
-class Users extends Section implements Initializable
-{
+class Users extends Section implements Initializable {
     /**
      * @var bool
      */
@@ -44,9 +43,8 @@ class Users extends Section implements Initializable
     /**
      * Initialize class.
      */
-    public function initialize()
-    {
-        $this->addToNavigation()->setPriority(150)->setIcon('fas fa-users');
+    public function initialize() {
+        $this->addToNavigation()->setPriority( 150 )->setIcon( 'fas fa-users' );
     }
 
     /**
@@ -54,88 +52,87 @@ class Users extends Section implements Initializable
      *
      * @return DisplayInterface
      */
-    public function onDisplay($payload = [])
-    {
+    public function onDisplay( $payload = [] ) {
         $columns = [
-            AdminColumn::text('id', '#')->setWidth('50px')->setHtmlAttribute('class', 'text-center'),
-            AdminColumn::link('name', 'Name', 'created_at')
-                ->setSearchCallback(function($column, $query, $search){
-                    return $query
-                        ->orWhere('name', 'like', '%'.$search.'%')
-                        ->orWhere('created_at', 'like', '%'.$search.'%')
-                    ;
-                })
-                ->setOrderable(function($query, $direction) {
-                    $query->orderBy('created_at', $direction);
-                })
+            AdminColumn::text( 'id', '#' )->setWidth( '50px' )->setHtmlAttribute( 'class', 'text-center' ),
+            AdminColumn::link( 'email', 'Email', 'created_at' )
+                       ->setSearchCallback( function ( $column, $query, $search ) {
+                           return $query
+                               ->orWhere( 'email', 'like', '%' . $search . '%' )
+                               ->orWhere( 'created_at', 'like', '%' . $search . '%' );
+                       } )
+                       ->setOrderable( function ( $query, $direction ) {
+                           $query->orderBy( 'created_at', $direction );
+                       } )
             ,
-            AdminColumn::text('email', 'Email'),
-            AdminColumn::boolean('name', 'On'),
-            AdminColumn::text('created_at', 'Created / updated', 'updated_at')
-                ->setWidth('160px')
-                ->setOrderable(function($query, $direction) {
-                    $query->orderBy('updated_at', $direction);
-                })
-                ->setSearchable(false)
+            AdminColumn::text( 'first_name', 'First name' ),
+            AdminColumn::text( 'last_name', 'Last name' ),
+            AdminColumn::text( 'created_at', 'Created / updated', 'updated_at' )
+                       ->setWidth( '160px' )
+                       ->setOrderable( function ( $query, $direction ) {
+                           $query->orderBy( 'updated_at', $direction );
+                       } )
+                       ->setSearchable( false )
             ,
         ];
 
         $display = AdminDisplay::datatables()
-            ->setName('firstdatatables')
-            ->setOrder([[0, 'asc']])
-            ->setDisplaySearch(true)
-            ->paginate(25)
-            ->setColumns($columns)
-            ->setHtmlAttribute('class', 'table-primary table-hover th-center')
-        ;
+                               ->setName( 'firstdatatables' )
+                               ->setOrder( [ [ 0, 'asc' ] ] )
+                               ->setDisplaySearch( true )
+                               ->paginate( 25 )
+                               ->setColumns( $columns )
+                               ->setHtmlAttribute( 'class', 'table-primary table-hover th-center' );
 
-        $display->setColumnFilters([
+        $display->setColumnFilters( [
             AdminColumnFilter::select()
-                ->setModelForOptions(\App\Models\User::class, 'name')
-                ->setLoadOptionsQueryPreparer(function($element, $query) {
-                    return $query;
-                })
-                ->setDisplay('name')
-                ->setColumnName('name')
-                ->setPlaceholder('All names')
+                             ->setModelForOptions( \App\Models\User::class, 'email' )
+                             ->setLoadOptionsQueryPreparer( function ( $element, $query ) {
+                                 return $query;
+                             } )
+                             ->setDisplay( 'email' )
+                             ->setColumnName( 'email' )
+                             ->setPlaceholder( 'All email' )
             ,
-        ]);
-        $display->getColumnFilters()->setPlacement('card.heading');
+        ] );
+        $display->getColumnFilters()->setPlacement( 'card.heading' );
 
         return $display;
     }
 
     /**
      * @param int|null $id
-     * @param array $payload
+     * @param array    $payload
      *
      * @return FormInterface
      */
-    public function onEdit($id = null, $payload = [])
-    {
-        $form = AdminForm::card()->addBody([
-            AdminFormElement::columns()->addColumn([
-                AdminFormElement::text('name', 'Name')
-                    ->required()
+    public function onEdit( $id = null, $payload = [] ) {
+        $form = AdminForm::card()->addBody( [
+            AdminFormElement::columns()->addColumn( [
+                AdminFormElement::text( 'first_name', 'First name' )
+                                ->required()
                 ,
-                AdminFormElement::html('<hr>'),
-                AdminFormElement::datetime('created_at')
-                    ->setVisible(true)
-                    ->setReadonly(false)
+                AdminFormElement::text( 'last_name', 'Last name' )
+                                ->required()
                 ,
-                AdminFormElement::html('last AdminFormElement without comma')
-            ], 'col-xs-12 col-sm-6 col-md-4 col-lg-4')->addColumn([
-                AdminFormElement::text('id', 'ID')->setReadonly(true),
-                AdminFormElement::html('last AdminFormElement without comma')
-            ], 'col-xs-12 col-sm-6 col-md-8 col-lg-8'),
-        ]);
+                AdminFormElement::html( '<hr>' ),
+            ], 'col-xs-12 col-sm-6 col-md-4 col-lg-4' )->addColumn( [
+                AdminFormElement::text( 'email', 'Email' )
+                                ->required()
+                ,
+                AdminFormElement::password( 'password', 'Password' )
+                                ->required()
+                                ->hashWithBcrypt()
+                ,
+            ], 'col-xs-12 col-sm-6 col-md-8 col-lg-8' ),
+        ] );
 
-        $form->getButtons()->setButtons([
-            'save'  => new Save(),
+        $form->getButtons()->setButtons( [
+            'save'            => new Save(),
             'save_and_close'  => new SaveAndClose(),
-            'save_and_create'  => new SaveAndCreate(),
-            'cancel'  => (new Cancel()),
-        ]);
+            'save_and_create' => new SaveAndCreate(),
+            'cancel'          => ( new Cancel() ),
+        ] );
 
         return $form;
     }
@@ -143,24 +140,21 @@ class Users extends Section implements Initializable
     /**
      * @return FormInterface
      */
-    public function onCreate($payload = [])
-    {
-        return $this->onEdit(null, $payload);
+    public function onCreate( $payload = [] ) {
+        return $this->onEdit( null, $payload );
     }
 
     /**
      * @return bool
      */
-    public function isDeletable(Model $model)
-    {
+    public function isDeletable( Model $model ) {
         return true;
     }
 
     /**
      * @return void
      */
-    public function onRestore($id)
-    {
+    public function onRestore( $id ) {
         // remove if unused
     }
 }
