bc0ddb8ff404edc3a8501ce9f8322a85c1ae92db
Bogdan <bogbuk@gmail.com>
2021-11-24 21:08:19 +0200

    fix 46

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,9 +18,9 @@
                     </div>
                     <div class="homehead__content">
                         <h1>{!! __("home.home_title") !!}</h1>
-{{--                        <div class="homehead__content-label"><i><img src="/img/logo-min.svg" alt="IMG"></i>--}}
-{{--                            <div>{!! __("home.home_sub_title") !!}</div>--}}
-{{--                        </div>--}}
+                        {{--                        <div class="homehead__content-label"><i><img src="/img/logo-min.svg" alt="IMG"></i>--}}
+                        {{--                            <div>{!! __("home.home_sub_title") !!}</div>--}}
+                        {{--                        </div>--}}
                     </div>
                 </div>
             </div>
@@ -41,10 +41,70 @@
                 </div>
             </div>
         </div>
-        <section class="b-video"
-                 data-fancybox="" data-src="{{$content['embed_video'] ?? 'https://www.youtube.com/watch?v=NpEaa2P7qZI'}}"
-                 style="cursor:pointer; background: url({{ strlen($content['block_image']) > 0  ? $content['block_image'] : '../img/video-bg.jpg'}}) center center no-repeat; background-size: cover;
-                     }">
+        <section class="b-video_old"
+            {{--                 data-fancybox="" --}}
+            {{--                 data-src="{{$content['embed_video'] ?? 'https://www.youtube.com/watch?v=NpEaa2P7qZI'}}"--}}
+            {{--                 style="cursor:pointer; background: url({{ strlen($content['block_image']) > 0  ? $content['block_image'] : '../img/video-bg.jpg'}}) center center no-repeat; background-size: cover;}"--}}
+        >
+            <div class="video_box" style="text-align: center">
+                <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+{{--                <div id="player"></div>--}}
+
+{{--                <script>--}}
+{{--                    // 2. This code loads the IFrame Player API code asynchronously.--}}
+{{--                    var tag = document.createElement('script');--}}
+
+{{--                    tag.src = "https://www.youtube.com/iframe_api";--}}
+{{--                    var firstScriptTag = document.getElementsByTagName('script')[0];--}}
+{{--                    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);--}}
+
+{{--                    // 3. This function creates an <iframe> (and YouTube player)--}}
+{{--                    //    after the API code downloads.--}}
+{{--                    var player;--}}
+
+{{--                    function onYouTubeIframeAPIReady() {--}}
+{{--                        player = new YT.Player('player', {--}}
+{{--                            height: '360',--}}
+{{--                            width: '640',--}}
+{{--                            videoId: 'l8bc3Ffqt0Q',--}}
+{{--                            events: {--}}
+{{--                                'onReady': onPlayerReady,--}}
+{{--                                'onStateChange': onPlayerStateChange--}}
+{{--                            }--}}
+{{--                        });--}}
+{{--                    }--}}
+
+{{--                    // 4. The API will call this function when the video player is ready.--}}
+{{--                    function onPlayerReady(event) {--}}
+{{--                        console.log('onPlayerReady');--}}
+{{--                        event.target.playVideo();--}}
+{{--                    }--}}
+
+{{--                    // 5. The API calls this function when the player's state changes.--}}
+{{--                    //    The function indicates that when playing a video (state=1),--}}
+{{--                    //    the player should play for six seconds and then stop.--}}
+{{--                    var done = false;--}}
+
+{{--                    function onPlayerStateChange(event) {--}}
+
+{{--                        console.log(event.data);--}}
+{{--                        if (event.data !== 1) {--}}
+{{--                            event.target.playVideo()--}}
+{{--                        }--}}
+{{--                        console.log('onPlayerStateChange: ', event);--}}
+{{--                        if (event.data == YT.PlayerState.PLAYING && !done) {--}}
+{{--                            setTimeout(stopVideo, 6000);--}}
+{{--                            done = true;--}}
+{{--                        }--}}
+{{--                    }--}}
+
+{{--                    function stopVideo() {--}}
+{{--                        player.stopVideo();--}}
+{{--                    }--}}
+{{--                </script>--}}
+
+                                {!! $content['embed_video'] !!}
+            </div>
             <div class="container">
                 <h2>{!! $content['video_block_title'] ?? '' !!}</h2>
             </div>
