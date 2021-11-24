e7658b65c595767d7ae3ab393289ecfb3009700d
Bogdan <bogbuk@gmail.com>
2021-11-24 12:03:34 +0200

    fix 44

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -85,7 +85,7 @@ class HomeController extends Controller {
             $placesResponse[] = [
                 'id' => $item['id'],
                 'title' => $this->getTranslateContent($item, 'title'),
-                'body' => substr(strip_tags($this->getTranslateContent($item, 'body')),0, 110) . '...',
+                'body' => substr(strip_tags($this->getTranslateContent($item, 'body')),0, 110) . (strlen(strip_tags($this->getTranslateContent($item, 'body'))) > 110) ? '...' : '',
                 'image' => $item['image'],
                 'durations' => $item['durations'],
                 'extra_durations' => $item['extra_durations'],
