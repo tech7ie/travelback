af700b15e9bf2dc76f692ee304aecfb1143ac72f
Bogdan <bogbuk@gmail.com>
2021-11-29 13:50:04 +0200

    fix css img

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -257,11 +257,10 @@
                     <div class="form-block">
                         <div class="form-block__wrap">
                             <div class="label">{!! __("widgets.calculator.title") !!}</div>
-{{--                            <div class="homehead-form form-vue calc" id="homehead-form">--}}
-{{--                                <v-calculator :request="{{json_encode($_GET)}}" :short="true" routes="{{$routes ?? []}}">--}}
-
-{{--                                </v-calculator>--}}
-{{--                            </div>--}}
+                            <div class="homehead-form form-vue calc" id="homehead-form">
+                                <v-calculator :request="{{json_encode($_GET)}}" :short="true" mode="home" routes="{{$routes ?? []}}">
+                                </v-calculator>
+                            </div>
                         </div>
                     </div>
                 </div>
