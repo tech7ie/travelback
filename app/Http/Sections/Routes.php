9dd996f2e531591ad4b3a7645c1f9a45d7d04474
Bogdan <bogbuk@gmail.com>
2021-12-01 09:52:12 +0200

    fix css

--- app/Http/Sections/Routes.php
+++ app/Http/Sections/Routes.php
@@ -205,7 +205,7 @@ class Routes extends Section implements Initializable {
      * @return bool
      */
     public function isDeletable( Model $model ) {
-        return falseы;
+        return false;
     }
 
     /**
