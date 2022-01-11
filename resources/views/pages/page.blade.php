3d537b3ddf91fba1c07d30d4a294587cb0e839a0
Bogdan <bogbuk@gmail.com>
2022-01-11 18:38:46 +0200

    fix 10

--- resources/views/pages/page.blade.php
+++ resources/views/pages/page.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="page">
         <div class="container">
-            <h2 style="padding-top: 20px">{!! $content['title'] ?? '' !!}</h2>
+            <h2 style="padding-top: 20px; text-align: center">{!! $content['title'] ?? '' !!}</h2>
             <p>{!! $content['body'] ?? '' !!}</p>
         </div>
     </section>
