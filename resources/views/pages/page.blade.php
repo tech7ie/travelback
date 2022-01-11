ca37d808fde185d5736be59679ef59c08ca0ae42
Bogdan <bogbuk@gmail.com>
2022-01-11 18:37:21 +0200

    fix 10

--- resources/views/pages/page.blade.php
+++ resources/views/pages/page.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <section class="page">
         <div class="container">
+            <р2 style="padding-top: 20px">{!! $content['title'] ?? '' !!}</р2>
             <p>{!! $content['body'] ?? '' !!}</p>
         </div>
     </section>
